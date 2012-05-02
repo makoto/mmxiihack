d7e85f7ce0e2576c6b3053f596f0cc93e1ad4dc6
Makoto Inoue <inouemak@googlemail.com>
2012-05-02 14:39:28 +0100

    Added http auth

--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == [ENV['USERNAME'], ENV['PASSWORD']]
+end
+
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
