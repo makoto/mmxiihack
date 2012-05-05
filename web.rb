0278f39bf892ab4f7453ab5701190bed52698185
Tatsuya Ono <ononoma@gmail.com>
2012-05-05 11:05:00 +0100

    make pushstate work nicely with sinatra

--- web.rb
+++ web.rb
@@ -4,6 +4,6 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == [ENV['USERNAME'], ENV['PASSWORD']]
 end
 
-get '/' do
+get %r{/|/intro|/info|/faq|/partners|/credit} do
   File.read(File.join('public', 'index.html'))
 end
