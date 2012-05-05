e0e5b339aae9b9c6770ffa868f9481aedc92b7d7
Tatsuya Ono <ononoma@gmail.com>
2012-05-05 10:35:39 +0100

    push state for links for permlink.

--- public/js/mmxiihack.js
+++ public/js/mmxiihack.js
@@ -1,13 +1,15 @@
 jQuery(function($){
+
+  // ============== Initialize
   $.supersized({
     slides: [ {image : 'bg/6764486061_c7c9b598a1_o.jpg', title : 'Image Credit: Maria Kazvan'} ]
   });
 
-  var contents = [{key: 'intro', path: '/', label: 'Introduction'},
-                  {key: 'info', path: '/info', label: 'Information'},
-                  {key: 'faq', path: '/faq', label: 'FAQ'},
-                  {key: 'partners', path: '/partners', label: 'Supporters'},
-                  {key: 'credit', path: '/credit', label: 'Credit'}];
+  var contents = [{key: 'intro', path: 'intro', label: 'Introduction'},
+                  {key: 'info', path: 'info', label: 'Information'},
+                  {key: 'faq', path: 'faq', label: 'FAQ'},
+                  {key: 'partners', path: 'partners', label: 'Supporters'},
+                  {key: 'credit', path: 'credit', label: 'Credit'}];
 
   var aniEvent = 'animationend webkitAnimationEnd MSAnimationEnd';
 
@@ -15,18 +17,36 @@ jQuery(function($){
     $('.hexagon.' + c.key).tooltip({title: c.label});
   });
 
+  // ============== Event handler
   $('.hexagon').click( function() {
     $(this).tooltip('hide');
+
     if ($(this).hasClass('select')) {
       unselectContent(true);
     } else {
-      var cnt = contentByHexagon($(this));
-      if (cnt) {
-        unselectContent(false);
-        selectContent(cnt);
-      }
+      var content = contentByHexagon($(this));
+      unselectContent(false);
+      selectContent(content);
+
+      setHistory(content);
     }
+
+    return false;
   });
+
+  // ============== Actions
+  function setHistory(content) {
+    if (!history.pushState) return false;
+
+    var url = "";
+    var title = "Londinium MMXII Hackathon";
+    if (content) {
+      url = content.path;
+      title = content.label + ": " + title;
+    }
+
+    history.pushState(content, title, url);
+  }
 
   function unselectContent(hideContents) {
     $('.hexagon').removeClass('select');
