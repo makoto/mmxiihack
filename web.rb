6850e781a7784593612ce06dc53660128509a27b
Makoto Inoue <inouemak@googlemail.com>
2012-05-09 21:22:14 +0100

    Added redirect

--- web.rb
+++ web.rb
@@ -4,6 +4,10 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == [ENV['USERNAME'], ENV['PASSWORD']]
 end
 
+get "*", :host_name => /^www\./ do
+  redirect request.url.gsub(/www\./,''), 301
+end
+
 get %r{/|/intro|/info|/faq|/partners|/credit} do
   File.read(File.join('public', 'index.html'))
 end
